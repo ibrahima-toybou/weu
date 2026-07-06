c9d98589d90bd0711cd02209cf25f4b7c12b515f
ibrah <ibrahima.toybou@coursdiderot.com>
2026-07-06 23:48:20 +0200

    ajout du mod offline

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -45,8 +45,8 @@ export default function RootLayout() {
         }}
       >
         <Stack.Screen name="index" />
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="(agent)" />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(agent)" options={{ headerShown: false }} />
       </Stack>
     </>
   );
